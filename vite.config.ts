85ffcf7f6223563145ac6ad28f90f73678964a9b
r-4u-1 <114141854+r-4u-1@users.noreply.github.com>
2025-08-11 00:03:39 +0200

    fix: vite conf fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfool-io/', 
 })
-
-export default {
-  base: '/portfool-io/', // required for project pages
-}
